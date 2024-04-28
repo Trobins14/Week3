584beb4ca606e7510793bad196910fd3c2e9dcf2
trobins14 <tristan.robins14@gmail.com>
2024-04-28 19:34:34 -0400

    Swapped parseFloat() for .toFixed()

--- excercises/Demos/ifScripts/pay_rules.js
+++ excercises/Demos/ifScripts/pay_rules.js
@@ -1,11 +1,11 @@
 //pay rules exercise
 let payRate;
-let hoursWorked;
+let hoursWorked = 53;
 
 if(hoursWorked > 40){
-    console.log(`Your pay rate is $${parseFloat(17.30)}`)
+    console.log(`Your pay rate is $${17.30.toFixed(2)}`)
 }else if(hoursWorked == 40){
-    console.log(`your pay rate is $${parseFloat()}`)
+    console.log(`your pay rate is $${25.00.toFixed(2)}`)
 }else if(hoursWorked < 40){
-    console.log(`Your pay rate is $${parseFloat(12.50)}`)
-}
+    console.log(`Your pay rate is $${12.50.toFixed(2)}`)
+};
