f933cab5578c4b78ae523d6a88608baa12087b54
trobins14 <tristan.robins14@gmail.com>
2024-04-28 19:53:42 -0400

    tested functionality with years "1900, 1950, 1999, 2000, 2001, 2012". Script accurately depicted whether the year was a leap year or not.

--- excercises/Demos/ifScripts/gregorianCalendar.js
+++ excercises/Demos/ifScripts/gregorianCalendar.js
@@ -1,6 +1,6 @@
 // console.log(Number.isInteger(1900.93));
 //console.log(Number.isInteger(1900/4));
-let year = 1900;
+let year = 2012;
 let yearMath = Number.isInteger(year / 4);
 if (yearMath == true) {
     console.log(`${year} is a leap year!`)
