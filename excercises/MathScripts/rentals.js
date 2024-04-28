02e9eb921912b9126418cffa1aaf3776151e9a22
trobins14 <tristan.robins14@gmail.com>
2024-04-28 17:07:17 -0400

    Added more variables and completed the final console log string.

--- excercises/MathScripts/rentals.js
+++ excercises/MathScripts/rentals.js
@@ -6,5 +6,11 @@ let vanCost = 250;
 
 let vanAmount = Math.ceil(people/vanSeats);
 // console.log(Math.ceil(vanAmount));
-console.log(vanAmount);
+// console.log(vanAmount);
+let costPerPerson = Math.ceil((vanAmount * vanCost) / 38);
+// console.log(costPerPerson.toFixed(2));
+let amountCollected = costPerPerson * people;
+let vanOrderCost = vanCost * vanAmount;
+let leftover = amountCollected - vanOrderCost;
+console.log(`Each person will have to pay $${costPerPerson}. The total amount collected was $${amountCollected}, and the vans were $${vanOrderCost}. The leftover amount is $${leftover}.`);
 
