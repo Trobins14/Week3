e302641c98f369cb846647e8d36c84e7aabbf7cb
trobins14 <tristan.robins14@gmail.com>
2024-04-28 19:30:59 -0400

    Created base if statements

---
+++ excercises/Demos/ifScripts/pay_rules.js
@@ -0,0 +1,11 @@
+//pay rules exercise
+let payRate;
+let hoursWorked;
+
+if(hoursWorked > 40){
+    console.log(`Your pay rate is $${parseFloat(17.30)}`)
+}else if(hoursWorked == 40){
+    console.log(`your pay rate is $${parseFloat()}`)
+}else if(hoursWorked < 40){
+    console.log(`Your pay rate is $${parseFloat(12.50)}`)
+}
