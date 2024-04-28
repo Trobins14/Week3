938708d05247a78a430f518329beb6b2ed355d66
trobins14 <tristan.robins14@gmail.com>
2024-04-28 15:24:36 -0400

    created baseline variables

---
+++ excercises/MathScripts/rentals.js
@@ -0,0 +1,10 @@
+//rentals calcs
+
+let people = 38;
+let vanSeats = 15;
+let vanCost = 250;
+
+let vanAmount = Math.ceil(people/vanSeats);
+// console.log(Math.ceil(vanAmount));
+console.log(vanAmount);
+
