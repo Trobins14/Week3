05c2b43029e41a8b17f566dfc76e483ce715d6d6
trobins14 <tristan.robins14@gmail.com>
2024-04-28 19:51:06 -0400

    Tested if 1900 was a leap year. Test resulted in a success.

--- excercises/Demos/ifScripts/gregorianCalendar.js
+++ excercises/Demos/ifScripts/gregorianCalendar.js
@@ -1,2 +1,9 @@
 // console.log(Number.isInteger(1900.93));
-// console.log(Number.isInteger(1900));
+//console.log(Number.isInteger(1900/4));
+let year = 1900;
+let yearMath = Number.isInteger(year / 4);
+if (yearMath == true) {
+    console.log(`${year} is a leap year!`)
+} else {
+    console.log(`${year} is NOT a leap year!`)
+}
