54d8d620b2fdff8833c176c8c97b189d98c9846a
trobins14 <tristan.robins14@gmail.com>
2024-04-28 20:36:44 -0400

    Created department converter. committing.

---
+++ excercises/switchScripts/dept_converter.js
@@ -0,0 +1,28 @@
+// deptartment converter
+let departmentName = 5;
+switch(departmentName){
+    case 1:
+        departmentName = Marketing;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+    case 5:
+        departmentName = Human Resources;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+    case 10:
+        departmentName = Accounting;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+    case 12:
+        departmentName = Legal;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+    case 18:
+        departmentName = IT;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+    case 20:
+        departmentName = Customer Relations;
+        console.log(`You are in the ${departmentName}!`);
+        break;
+}
