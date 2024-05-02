b08e5b17fb6d6661fd4e6f2d6438eee975326d65
trobins14 <tristan.robins14@gmail.com>
2024-05-02 10:56:54 -0400

    completed the exercise

--- excercises/Strings/advanced_parsing.js
+++ excercises/Strings/advanced_parsing.js
@@ -7,6 +7,12 @@ let name = "Toi Girl Ramey";
 
 function parseAndDisplayName(g){
     let spacePos = g.indexOf(" ");
-    console.log(`Name: g`);
-    console.log(`First Name: ${g.slice(0, spacePos)}`)
+    let spacePos2 = g.lastIndexOf(" ");
+    console.log(`Name: ${g}`);
+    console.log(`First Name: ${g.slice(0, spacePos)}`);
+    console.log(`Middle Name:${g.slice(spacePos, spacePos2)}`);
+    console.log(`Last Name:${g.slice(spacePos2, -1)}${g.slice(-1)}`);
 }
+parseAndDisplayName("Jamie Quagsire Popper");
+
+// for(let i = g.indexOf(" "), i )
