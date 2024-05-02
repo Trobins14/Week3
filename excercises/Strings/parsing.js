9d045dd6d0305faaf1e477ace7ec312d346181f0
trobins14 <tristan.robins14@gmail.com>
2024-05-01 21:26:15 -0400

    created parsing js

---
+++ excercises/Strings/parsing.js
@@ -0,0 +1,24 @@
+let fullName = "Reyna Kaye";
+// console.log(fullName.indexOf(" "));
+// console.log(`Name: ${fullName.slice(0, 10)}`);
+// console.log(`First Name: ${fullName.slice(0, 4)}`);
+// console.log(`Last Name: ${fullName.slice(6, 10)}`);
+// let firstName = fullName.slice(0, 4);
+// let lastName = fullName.slice(6, 10);
+// let fullNameSlice = fullName.slice(0, 10);
+
+// function parseAndDisplayName(f, g){
+//     console.log(`Name: ${fullName}`);
+//     console.log(`First Name: ${f}`);
+//     console.log(`Last name: ${g}`);
+// };
+
+function parseAndDisplayName(){
+    console.log(`Name: ${fullName.slice(0, 10)}`);
+    console.log(`First Name: ${fullName.slice(0, )}`);
+    console.log(`Last Name: ${fullName.slice(6, 10)}`);
+}
+
+parseAndDisplayName(fullName);
+
+
